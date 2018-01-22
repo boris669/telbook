f2cbfefbd386c0265898853f75c66d973c6eb3f0
Nikita Somenkov <somenkov.nikita@icloud.com>
2018-01-22 21:36:56 +0300

    Fix russian

--- telbook.c
+++ telbook.c
@@ -4,9 +4,11 @@
 *   @date   20 Jan 2018
 */
 #include <stdio.h>
+#include <locale.h>
 
 int main(int argc, char *argv[])
 {
+    setlocale(LC_ALL, "Rus");
     if (argc != 2)
     {
         printf("требуется файл");
