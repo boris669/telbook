5527d01c87c000d9ac8143fff23ee13247fc514e
Nikita Somenkov <somenkov.nikita@icloud.com>
2018-01-23 23:15:56 +0300

    Build error

--- telbook.c
+++ telbook.c
@@ -3,28 +3,31 @@
 *   @email somenkov.nikita@icloud.com
 *   @date   20 Jan 2018
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main (int argc, char *argv[])
+
+
+int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
+    if (argc != 2) {
         printf("требуется файл");
         return 2;
     }
 
     FILE *file = fopen(argv[1], "r");
-    if (file != 0)
-    {
+    if (file != 0) {
         printf("File not open");
         return 1;
     }
+}
+
 void add_strings(float *str[], size_t count_x)
- {
+{
     for (size_t i = 0; i < count_x; i++)
     {
-      str = str[count_x + i];
+        str = str[count_x + i];
         for (int j = 0; j < 1024; ++j)
         {
             const size_t index = count_x + i;
@@ -34,5 +37,4 @@ void add_strings(float *str[], size_t count_x)
         }
 
     }
- }
 }
