10a35ba22e595b6ece0e51344c803b3ce627dce1
Nikita Somenkov <somenkov.nikita@icloud.com>
2018-01-27 18:07:52 +0300

    Add files and parser module

---
+++ parser.c
@@ -0,0 +1,8 @@
+/**
+*   @author Nikita Somenkov
+*   @email somenkov.nikita@icloud.com
+*   @date   27 Jan 2018
+*/
+
+#include "parser.h"
+
---
+++ parser.h
@@ -0,0 +1,7 @@
+/**
+*   @author Nikita Somenkov
+*   @email somenkov.nikita@icloud.com
+*   @date   27 Jan 2018
+*/
+
+#pragma once
